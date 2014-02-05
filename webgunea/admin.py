9a284541e81a285686c1dbe5102636d8c3272419
gorka ercilla <ercillagorka@gmail.com>
2014-02-05 17:34:14 +0100

    admin-ean ekintzak agertzen diren modua aldatu dot

--- webgunea/admin.py
+++ webgunea/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webgunea.models import *
 
-# Register your models here.
+class EkintzaAdmin(admin.ModelAdmin):
+    list_display = ('tituloa', 'egune', 'lekua', 'deskribapena','kartela',)
+    list_filter = ('egune','sortzailea')
+    filter_horizontal = ('sortzailea',)
+
+
+class AutorAdmin(admin.ModelAdmin):
+	list_display = ('nor','email','webgunea')
+
+admin.site.register(Autor,AutorAdmin)
+admin.site.register(Ekintza,EkintzaAdmin)
